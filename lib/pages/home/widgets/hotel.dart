ece414b9f3cac71680d067849bc58f03b812d410
victoireMyinda <myndavictoire@gmail.com>
2023-11-12 16:58:04 +0100

    main: add_filter_hotels_widget_in_hotel_widget

--- lib/pages/home/widgets/hotel.dart
+++ lib/pages/home/widgets/hotel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hotels/pages/home/home.dart';
 
 class Hotel extends StatelessWidget {
   const Hotel({super.key});
@@ -6,8 +8,34 @@ class Hotel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(10),
       height: 2000,
       color: Colors.white,
+      child: Column(children: [
+        Container(
+          height: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "550 hotels founds",
+                style: GoogleFonts.nunito(color: Colors.black, fontSize: 15),
+              ),
+              Row(
+                children: [
+                  Text("Filter",
+                      style: GoogleFonts.nunito(
+                          color: Colors.black, fontSize: 15)),
+                  const IconButton(
+                      onPressed: null,
+                      icon: Icon(Icons.filter_list_outlined,
+                          color: dgreen, size: 25))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
